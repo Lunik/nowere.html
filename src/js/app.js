9223c826d13ba6729fefa223693176b45be2ad0e
lunik <guillaume.lunik@gmail.com>
2018-08-13 21:59:49 +0200

    init

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+;(function(){
+	
+	//if (window.location.host === "lunik.xyz") {
+		let selector = document.querySelectorAll(".crypto, .certificate")
+		for (let div of selector) {
+			div.style.display = "block"
+		}
+	//}
+})()
